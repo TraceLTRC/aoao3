50e93f904a091e912fea221fb94e5272f07f577b
TraceL <kivlanbahmidt@gmail.com>
2023-01-19 17:59:55 +0700

    Add tests

--- functions/archivefanfic/main.go
+++ functions/archivefanfic/main.go
@@ -1,6 +1,7 @@
 package archivefanfic
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -11,6 +12,22 @@ func init() {
 	functions.HTTP("archivefanfic", ArchiveFanfic)
 }
 
-func ArchiveFanfic(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(res, "Hello, world!\nYou accessed archivefanfic function!")
+func ArchiveFanfic(w http.ResponseWriter, r *http.Request) {
+	var b struct {
+		Name string
+	}
+
+	switch r.Method {
+	case "GET":
+		fmt.Fprintf(w, "You did a get request to %s!", r.URL)
+	case "POST":
+		err := json.NewDecoder(r.Body).Decode(&b)
+		if err != nil {
+			fmt.Fprintf(w, "Failed to decode, %v", err)
+		}
+
+		fmt.Fprintf(w, "You did a post request, %s!", b.Name)
+	default:
+		fmt.Fprintf(w, "You did a %s request!", r.Method)
+	}
 }
